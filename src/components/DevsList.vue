54f8c8542e3aec59e9966288a067a86d95582595
Manu <operamike@mail.ru>
2021-09-08 22:39:17 +0200

    Init

--- src/components/DevsList.vue
+++ src/components/DevsList.vue
@@ -4,8 +4,11 @@
       <li v-for="dev in devs" :key="dev.id" style="cursor: pointer" @click="index = dev.id">
         {{ dev.name }}
         <div v-if="dev.id === index">
+          <hr>
+          <h3>{{ dev.name }} details:</h3>
           Age: {{ dev.age }}
           Gender: {{ dev.gender }}
+          <hr>
         </div>
       </li>
     </ul>
